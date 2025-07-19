cb812a7b3c0fb68e29431ec694a7f949d9bbe589
fauxfox117 <SABolin1@me.com>
2025-07-19 14:02:56 -0400

    Header/Footer set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     <header className="header">
       <div className="header__navbar">
         <img className="header__logo"></img>
-        <ul className="header__nav-list">
-          <li>Memberships</li>
-          <li>Trails</li>
-          <li>Login</li>
-          <li>Sign Up</li>
+        <ul className="header__nav">
+          <li className="header__nav-list">Memberships</li>
+          <li className="header__nav-list">Trails</li>
+          <li className="header__nav-list">Login</li>
+          <li className="header__nav-list">Sign Up</li>
         </ul>
         <h1 className="header__title">Best Summer Sightseeing Roadtrip</h1>
       </div>
