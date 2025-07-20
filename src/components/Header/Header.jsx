7a9204e5ccf5b6378436dc2c012faf58b84436e7
fauxfox117 <SABolin1@me.com>
2025-07-20 14:13:46 -0400

    card updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,23 +4,10 @@ import Logo from "../../../public/logo.svg";
 const Header = () => {
   return (
     <header className="header">
-<<<<<<< HEAD
-      <div className="header__navbar">
-        <img className="header__logo"></img>
-        <ul className="header__nav">
-          <li className="header__nav-list">Memberships</li>
-          <li className="header__nav-list">Trails</li>
-          <li className="header__nav-list">Login</li>
-          <li className="header__nav-list">Sign Up</li>
-        </ul>
-        <h1 className="header__title">Best Summer Sightseeing Roadtrip</h1>
-      </div>
-=======
       <h1 className="header__title">Best Summer Sightseeing Roadtrips</h1>
       <p className="header__subtitle">
         Discover the most picturesque landmarks and natural attractions
       </p>
->>>>>>> main
     </header>
   );
 };
