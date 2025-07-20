78ef5509478fe443a3f43a837c525a793578eebd
ValerieTsan <valeriejean.tsan@gmail.com>
2025-07-20 05:03:55 -0400

    fix header and main ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,10 @@ import Logo from "../../../public/logo.svg";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__navbar">
-        <img className="header__logo" src={Logo} alt="Logo" />
-        <h1 className="header__title">Best Summer Sightseeing Roadtrips</h1>
-        <nav className="header__desktop-nav">
-          <ul className="header__nav-list"></ul>
-        </nav>
-      </div>
+      <h1 className="header__title">Best Summer Sightseeing Roadtrips</h1>
+      <p className="header__subtitle">
+        Discover the most picturesque landmarks and natural attractions
+      </p>
     </header>
   );
 };
