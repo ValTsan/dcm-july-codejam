32df1caf58964e2c11b2fa42d27964353d8a2460
fauxfox117 <SABolin1@me.com>
2025-07-20 21:05:20 -0400

    basic navbar push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import "./Header.css";
-import Logo from "../../../public/logo.svg";
+import Logo from "../../../public/header-logo.png";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__navbar">
+        <img className="header__nav-logo" src={Logo}></img>
         <ul className="header__nav">
           <li className="header__nav-list">About</li>
+          {/* separate pages for about and trails, trails can be a dead link if needed */}
           <li className="header__nav-list">Trails</li>
           <li className="header__nav-list">Log In</li>
+          {/* implementing a modal pop up for both log in and sign up */}
           <li className="header__nav-list">Sign Up</li>
         </ul>
       </div>
