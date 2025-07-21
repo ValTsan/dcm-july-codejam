b2e7fa8f23faa6159355d0866a6c9979d220990b
fauxfox117 <SABolin1@me.com>
2025-07-20 20:36:09 -0400

    navbar started/UI implements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,14 @@ import Logo from "../../../public/logo.svg";
 const Header = () => {
   return (
     <header className="header">
+      <div className="header__navbar">
+        <ul className="header__nav">
+          <li className="header__nav-list">About</li>
+          <li className="header__nav-list">Trails</li>
+          <li className="header__nav-list">Log In</li>
+          <li className="header__nav-list">Sign Up</li>
+        </ul>
+      </div>
       <h1 className="header__title">Best Summer Sightseeing Roadtrips</h1>
       <p className="header__subtitle">
         Discover the most picturesque landmarks and natural attractions
