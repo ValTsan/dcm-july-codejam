decddafed750f94b601d9a6a4f1cbac6329af5a4
ValerieTsan <valeriejean.tsan@gmail.com>
2025-07-23 02:56:03 -0400

    Fix footer css and clean up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import Logo from "/header-logo.png";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer__container">
-        Best Summer Sightseeing Roadtrip {"\u2122"}
+      <div className="footer__left">
+        <img src={Logo} alt="Daytrppr Logo" className="footer__logo" />
+        <span className="footer__title">Daytrippr&trade;</span>
+      </div>
+
+      <div className="footer__center">
+        Best Summer Sightseeing Roadtrip&trade;
+      </div>
+
+      <div className="footer__right">
+        Built by DCM Team Valerie, Jeel, Viktor and Steven
       </div>
     </div>
   );
